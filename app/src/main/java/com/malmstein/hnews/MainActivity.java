0321caaaaa6e11e51ad84768cd81b34ee1ddf311
David Gonzalez <malmstein@gmail.com>
2014-11-29 15:14:55 +0000

    basic tabs and colors

--- app/src/main/java/com/malmstein/hnews/MainActivity.java
+++ app/src/main/java/com/malmstein/hnews/MainActivity.java
@@ -1,30 +1,48 @@
 package com.malmstein.hnews;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setToolbar();
+    }
+
+    private void setToolbar(){
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        setAppBarColor(getDefaultAppBarColor(getResources()));
+    }
+
+    private void setAppBarColor(int color) {
+        if (toolbar != null) {
+            toolbar.setBackgroundColor(color);
+        }
+    }
+
+    private int getDefaultAppBarColor(Resources resources) {
+        return resources.getColor(R.color.orange);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
---
+++ app/src/main/java/com/malmstein/hnews/MainNavigationAdapter.java
@@ -0,0 +1,29 @@
+package com.malmstein.hnews;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class MainNavigationAdapter extends FragmentPagerAdapter {
+
+    private final String[] TITLES = {"News", "Comments", "Show", "Ask"};
+
+    public MainNavigationAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return TITLES[position];
+    }
+
+    @Override
+    public int getCount() {
+        return TITLES.length;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return DefaultCardFragment.newInstance(position);
+    }
+}
