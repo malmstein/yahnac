dc065143a65cccf28c0af6b5b0598f85a8c227e6
David Gonzalez <malmstein@gmail.com>
2015-01-15 18:55:17 +0000

    whop

--- app/src/main/java/com/malmstein/hnews/data/HNewsDbHelper.java
+++ app/src/main/java/com/malmstein/hnews/data/HNewsDbHelper.java
@@ -6,7 +6,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class HNewsDbHelper extends SQLiteOpenHelper {
 
-    private static final int DATABASE_VERSION = 2;
+    private static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "hnews.db";
 
     public HNewsDbHelper(Context context) {
