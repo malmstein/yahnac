05ba7bdc06a1b334d1b6201639aa80c54e960ecf
David Gonzalez <malmstein@gmail.com>
2015-01-25 17:13:30 +0000

    this had to be cleaned too

--- app/src/main/java/com/malmstein/hnews/inject/Inject.java
+++ app/src/main/java/com/malmstein/hnews/inject/Inject.java
@@ -7,22 +7,18 @@ import com.malmstein.hnews.feed.DatabasePersister;
 public class Inject {
 
     private static Inject INSTANCE;
-    private final CommentsProvider commentsProvider;
     private final DataRepository dataRepository;
 
-    private Inject(CommentsProvider commentsProvider, DataRepository dataRepository) {
-        this.commentsProvider = commentsProvider;
+    private Inject(DataRepository dataRepository) {
         this.dataRepository = dataRepository;
     }
 
     public static void using(DependenciesFactory factory) {
         DatabasePersister databasePersister = factory.createDatabasePersister();
 
-        CommentsProvider commentsProvider = factory.createCommentsProvider(databasePersister);
-
         DataRepository dataRepository = factory.createDataRepository(databasePersister);
 
-        INSTANCE = new Inject(commentsProvider, dataRepository);
+        INSTANCE = new Inject(dataRepository);
     }
 
     private static Inject instance() {
@@ -31,8 +27,6 @@ public class Inject {
         }
         return INSTANCE;
     }
-
-    public static CommentsProvider commentsProvider() { return instance().commentsProvider; }
 
     public static DataRepository dataRepository() {
         return instance().dataRepository;
