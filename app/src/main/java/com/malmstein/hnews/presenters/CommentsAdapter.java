a423bb66c08a4ce3d968f1a516fbc4f4bfdcc6af
David Gonzalez <malmstein@gmail.com>
2015-02-22 08:54:10 +0000

    adding new view type to comments adapter

--- app/src/main/java/com/malmstein/hnews/presenters/CommentsAdapter.java
+++ app/src/main/java/com/malmstein/hnews/presenters/CommentsAdapter.java
@@ -13,41 +13,90 @@ import com.malmstein.hnews.R;
 import com.malmstein.hnews.model.Comment;
 import com.novoda.notils.caster.Views;
 
-public class CommentsAdapter extends CursorRecyclerAdapter<CommentsAdapter.ViewHolder> {
+public class CommentsAdapter extends CursorRecyclerAdapter<RecyclerView.ViewHolder> {
+
+    private static int TYPE_HEADER = 0;
+    private static int TYPE_COMMENT = 1;
 
     public CommentsAdapter(Cursor cursor) {
         super(cursor);
     }
 
     @Override
-    public void onBindViewHolderCursor(ViewHolder holder, Cursor cursor) {
-        Comment comment = Comment.from(cursor);
-        holder.text.setText(Html.fromHtml(comment.getText()));
-        holder.text.setMovementMethod(LinkMovementMethod.getInstance());
-        holder.author.setText(comment.getBy());
-        holder.when.setText(comment.getTimeText());
-        holder.root.setPadding(comment.getLevel(), 0, 0, 0);
+    public int getItemViewType(int position) {
+        if (position == 0){
+            return TYPE_HEADER;
+        } else {
+            return TYPE_COMMENT;
+        }
     }
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.view_comment, parent, false);
-        ViewHolder vh = new ViewHolder(v);
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (viewType == TYPE_HEADER) {
+            return createHeaderHolder(parent);
+        } else {
+            return createCommentHolder(parent);
+        }
+    }
+
+    @Override
+    public void onBindViewHolderCursor(RecyclerView.ViewHolder holder, Cursor cursor) {
+        Comment comment = Comment.from(cursor);
+
+        if (holder instanceof CommentViewHolder) {
+            ((CommentViewHolder) holder).bind(comment);
+        } else {
+            ((HeaderViewHolder) holder).bind(comment);
+        }
+
+    }
+
+    private RecyclerView.ViewHolder createHeaderHolder(ViewGroup parent){
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.view_comment_header, parent, false);
+        HeaderViewHolder vh = new HeaderViewHolder(v);
         return vh;
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public static class HeaderViewHolder extends RecyclerView.ViewHolder {
+        public final TextView text;
+
+        public HeaderViewHolder(View view) {
+            super(view);
+            text = Views.findById(view, R.id.comment_header_text);
+        }
+
+        public void bind(Comment comment){
+            text.setText(Html.fromHtml(comment.getText()));
+        }
+    }
+
+    private RecyclerView.ViewHolder createCommentHolder(ViewGroup parent){
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.view_comment, parent, false);
+        CommentViewHolder vh = new CommentViewHolder(v);
+        return vh;
+    }
+
+    public static class CommentViewHolder extends RecyclerView.ViewHolder {
         public final TextView text;
         public final TextView author;
         public final TextView when;
         public final View root;
 
-        public ViewHolder(View view) {
+        public CommentViewHolder(View view) {
             super(view);
             text = Views.findById(view, R.id.comment_text);
             author = Views.findById(view, R.id.comment_by);
             when = Views.findById(view, R.id.comment_when);
             root = Views.findById(view, R.id.comment_root);
+        }
+
+        public void bind(Comment comment) {
+            text.setText(Html.fromHtml(comment.getText()));
+            text.setMovementMethod(LinkMovementMethod.getInstance());
+            author.setText(comment.getBy());
+            when.setText(comment.getTimeText());
+            root.setPadding(comment.getLevel(), 0, 0, 0);
         }
     }
 
