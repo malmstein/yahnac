ead8bf5457cbc07a750c58feb079d81be2f4a47d
David Gonzalez <malmstein@gmail.com>
2015-03-14 13:23:24 +0000

    fixing back navigation

--- app/src/main/java/com/malmstein/yahnac/HNewsActivity.java
+++ app/src/main/java/com/malmstein/yahnac/HNewsActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -98,4 +99,13 @@ public class HNewsActivity extends ActionBarActivity {
         return navigator;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home){
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+
+    }
 }
