20a13ea782e512b65d6e4dad70a0a30be1087651
David Gonzalez <malmstein@gmail.com>
2015-03-15 16:54:58 +0000

    more work

--- app/src/main/java/com/malmstein/yahnac/data/HNewsApi.java
+++ app/src/main/java/com/malmstein/yahnac/data/HNewsApi.java
@@ -2,16 +2,25 @@ package com.malmstein.yahnac.data;
 
 import android.content.ContentValues;
 
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 import com.malmstein.yahnac.model.StoriesJsoup;
 import com.malmstein.yahnac.model.Story;
 import com.malmstein.yahnac.tasks.FetchCommentsTask;
 import com.malmstein.yahnac.tasks.FetchStoriesTask;
 
 import java.io.IOException;
+import java.util.Map;
 import java.util.Vector;
 
 import rx.Observable;
+import rx.Observer;
 import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 
 public class HNewsApi {
@@ -87,6 +96,91 @@ public class HNewsApi {
                 subscriber.onError(e);
             }
             subscriber.onNext(commentsList);
+        }
+    }
+
+    public void getStories(Story.TYPE type) {
+
+        Vector<ContentValues> storiesList = new Vector<>();
+
+        Observable.create(new StoriesObserver(type))
+                .concatWith(new StoryObserver())
+                .flatMap(itemFunc)
+                .observeOn(AndroidSchedulers.mainThread());
+
+    }
+
+    private static class StoriesObserver implements Observable.OnSubscribe<Long> {
+
+        private final Story.TYPE type;
+
+        public StoriesObserver(Story.TYPE type) {
+            this.type = type;
+        }
+
+        @Override
+        public void call(final Subscriber<? super Long> subscriber) {
+            Firebase topStories = new Firebase("https://hacker-news.firebaseio.com/v0/topstories");
+            topStories.addChildEventListener(new ChildEventListener() {
+
+                @Override
+                public void onChildAdded(DataSnapshot snapshot, String previousChildKey) {
+                    Long itemId = (Long) snapshot.getValue();
+                    subscriber.onNext(itemId);
+                }
+
+                @Override
+                public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+                }
+
+                @Override
+                public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+                }
+
+                @Override
+                public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                }
+
+                @Override
+                public void onCancelled(FirebaseError firebaseError) {
+
+                }
+
+            });
+        }
+    }
+
+    private Func1 itemFunc = new Func1<Long, Observable<Vector<ContentValues>>>() {
+        @Override
+        public Observable<Vector<ContentValues>> call(Long itemId) {
+            Firebase item = new Firebase("https://hacker-news.firebaseio.com/v0/item/" + itemId);
+            item.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    Map<String, Object> newItem = (Map<String, Object>) dataSnapshot.getValue();
+
+                }
+
+                @Override
+                public void onCancelled(FirebaseError firebaseError) {
+
+                }
+            });
+        }
+    };
+
+    private static class StoryObserver implements Observable.OnSubscribe<Map<String, Object>> {
+
+        public StoryObserver(Map<String, Object> newItem) {
+
+        }
+
+        @Override
+        public void call(Subscriber<? super Map<String, Object>> subscriber) {
+
         }
     }
 
