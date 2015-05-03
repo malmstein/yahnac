d8466e431c35066224e274bc348a1e757a9ed55a
David Gonzalez <malmstein@gmail.com>
2015-05-03 20:54:34 +0100

    fixed login task

--- app/src/main/java/com/malmstein/yahnac/tasks/LoginTask.java
+++ app/src/main/java/com/malmstein/yahnac/tasks/LoginTask.java
@@ -1,27 +1,20 @@
 package com.malmstein.yahnac.tasks;
 
+import android.text.TextUtils;
+
 import com.malmstein.yahnac.model.Login;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
-import org.apache.http.Header;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 
 public class LoginTask {
 
-    private String LOGIN_PAGE_URL = "https://news.ycombinator.com/login";
-    private String LOGIN_URL = "https://news.ycombinator.com/x";
-
+    public static final String BASE_URL = "https://news.ycombinator.com";
+    public static final String USER_AGENT = System.getProperty("http.agent");
+    public static final int TIMEOUT_MILLIS = 40 * 1000;
+    private static final String LOGIN_URL_EXTENSION = "/login?go_to=news";
     private final String username;
     private final String password;
 
@@ -30,36 +23,33 @@ public class LoginTask {
         this.password = password;
     }
 
+    public static Connection anonConnect(String baseUrlExtension) {
+        Connection conn = Jsoup.connect(BASE_URL + baseUrlExtension)
+                .timeout(TIMEOUT_MILLIS)
+                .userAgent(USER_AGENT);
+        conn.header("Accept-Encoding", "gzip");
+
+        return conn;
+    }
+
     public Login execute() throws IOException {
 
-        try {
-            Document loginpage = Jsoup.connect(LOGIN_PAGE_URL).get();
-            String fnid = loginpage.select("input[name=fnid]").attr("value");
-            HttpClient httpclient = new DefaultHttpClient();
-            HttpPost httppost = new HttpPost(LOGIN_URL);
-            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
-            nameValuePairs.add(new BasicNameValuePair("fnid", fnid));
-            nameValuePairs.add(new BasicNameValuePair("u", username));
-            nameValuePairs.add(new BasicNameValuePair("p", password));
-            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-            httppost.addHeader("Content-type", "application/x-www-form-urlencoded");
-            httppost.addHeader("Accept", "text/plain");
-//            httppost.addHeader("User-agent", "Mozilla/5.0 (Linux; U; Android 2.2; en-us; Nexus One Build/FRF91) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1");
+        Connection.Response response = anonConnect("/login")
+                .data("go_to", "news")
+                .data("acct", username)
+                .data("pw", password)
+                .header("Origin", BASE_URL)
+                .followRedirects(true)
+                .referrer(BASE_URL + LOGIN_URL_EXTENSION)
+                .method(Connection.Method.POST)
+                .execute();
 
-            HttpResponse response = httpclient.execute(httppost);
-            Header[] cookies = response.getHeaders("Set-Cookie");
-            if (cookies.length > 0) {
-                String cookie = null;
-                for (int i = 0; i < cookies.length; i++) {
-                    cookie += cookies[i].getValue() + ';';
-                }
-                return new Login(username, cookie, Login.Status.SUCCESSFUL);
-            } else {
-                return new Login(username, null, Login.Status.WRONG_CREDENTIALS);
-            }
-        } catch (Exception e) {
-            return new Login(username, null, Login.Status.NETWORK_ERROR);
+        String cookie = response.cookie("user");
+
+        if (!TextUtils.isEmpty(cookie)) {
+            return new Login(username, cookie, Login.Status.SUCCESSFUL);
+        } else {
+            return new Login(username, null, Login.Status.WRONG_CREDENTIALS);
         }
-
     }
 }
