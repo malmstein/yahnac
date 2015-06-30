b709138bb2b6086818a5cdc6c761438d89a69561
David Gonzalez <malmstein@gmail.com>
2015-06-30 11:56:54 +0100

    we will use a view in the drawer header that will inflate a login box or the username

---
+++ app/src/main/java/com/malmstein/yahnac/drawer/NavigationDrawerHeader.java
@@ -0,0 +1,40 @@
+package com.malmstein.yahnac.drawer;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+
+import com.malmstein.yahnac.R;
+import com.malmstein.yahnac.updater.LoginSharedPreferences;
+
+public class NavigationDrawerHeader extends LinearLayout {
+
+    private LoginSharedPreferences loginSharedPreferences;
+
+    public NavigationDrawerHeader(Context context) {
+        super(context);
+    }
+
+    public NavigationDrawerHeader(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public NavigationDrawerHeader(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+
+        loginSharedPreferences = LoginSharedPreferences.newInstance();
+        if (loginSharedPreferences.isLoggedIn()) {
+            LayoutInflater.from(getContext()).inflate(R.layout.view_story_comment_header, this, true);
+        } else {
+
+        }
+
+    }
+
+}
