2ef0bb728c7820d780ee8a86b7718d20deb8d423
David Gonzalez <malmstein@gmail.com>
2015-07-10 18:30:23 +0100

    wrapping up the login logic

--- app/src/main/java/com/malmstein/yahnac/login/LoginActivity.java
+++ app/src/main/java/com/malmstein/yahnac/login/LoginActivity.java
@@ -2,15 +2,38 @@ package com.malmstein.yahnac.login;
 
 import android.os.Bundle;
 import android.support.v4.view.ViewCompat;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.malmstein.yahnac.HNewsActivity;
 import com.malmstein.yahnac.R;
+import com.malmstein.yahnac.data.Provider;
+import com.malmstein.yahnac.inject.Inject;
+import com.malmstein.yahnac.model.Login;
 import com.novoda.notils.caster.Views;
+
+import rx.Observer;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
 
 public class LoginActivity extends HNewsActivity {
 
     public static final String VIEW_TOOLBAR_TITLE = "login:toolbar:title";
+
+    private EditText usernameView;
+    private EditText passwordView;
+    private View errorView;
+    private TextView titleView;
+    private ProgressBar progressView;
+    private View loginView;
+    private View cancelView;
+
+    private InputFieldValidator inputFieldValidator = new InputFieldValidator();
+    private Subscription subscription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +44,110 @@ public class LoginActivity extends HNewsActivity {
 
         View toolbarTitle = Views.findById(this, R.id.dialog_login_title);
         ViewCompat.setTransitionName(toolbarTitle, VIEW_TOOLBAR_TITLE);
+
+        titleView = Views.findById(this, R.id.dialog_login_title);
+        errorView = Views.findById(this, R.id.dialog_login_error);
+        progressView = Views.findById(this, R.id.dialog_login_progress);
+        usernameView = Views.findById(this, R.id.dialog_login_username);
+        passwordView = Views.findById(this, R.id.dialog_login_password);
+        passwordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    if (validate()) {
+                        login();
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+        cancelView = Views.findById(this, R.id.dialog_login_cancel);
+        cancelView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        loginView = Views.findById(this, R.id.dialog_login_login);
+        loginView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (validate()) {
+                    login();
+                }
+            }
+        });
+
     }
+
+    private boolean validate() {
+        boolean isUsernameValid = inputFieldValidator.isValid(getUsername());
+        boolean isPasswordValid = inputFieldValidator.isValid(getPassword());
+        if (!isPasswordValid || !isUsernameValid) {
+            showError();
+        }
+        return isUsernameValid && isPasswordValid;
+    }
+
+    private void showError() {
+        errorView.setVisibility(View.VISIBLE);
+    }
+
+    private void login() {
+        showProgress();
+        Provider provider = Inject.provider();
+        subscription = provider
+                .observeLogin(getUsername(), getPassword())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Login.Status>() {
+                    @Override
+                    public void onCompleted() {
+                        if (!subscription.isUnsubscribed()) {
+                            subscription.unsubscribe();
+                        }
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Inject.crashAnalytics().logSomethingWentWrong("Provider: login: " + getUsername(), e);
+                    }
+
+                    @Override
+                    public void onNext(Login.Status status) {
+                        if (status == Login.Status.SUCCESSFUL) {
+                            finish();
+                        } else {
+                            hideProgress();
+                            showError();
+                        }
+                    }
+                });
+
+    }
+
+    protected String getPassword() {
+        return passwordView.getText().toString();
+    }
+
+    protected String getUsername() {
+        return usernameView.getText().toString();
+    }
+
+    private void showProgress() {
+        titleView.setText(R.string.title_checking_account);
+        progressView.setVisibility(View.VISIBLE);
+    }
+
+    private void hideProgress() {
+        titleView.setText(R.string.title_add_account);
+        progressView.setVisibility(View.INVISIBLE);
+    }
+
+    public interface Listener {
+        void onLoginSucceed();
+
+        void onLoginCancelled();
+    }
+
 }
