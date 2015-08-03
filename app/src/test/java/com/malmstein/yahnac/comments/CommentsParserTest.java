d73a54fa8e403b04ec2d78b0bef68329a68d161a
David Gonzalez <malmstein@gmail.com>
2015-08-03 17:36:04 +0100

    do you even test bro?

--- app/src/test/java/com/malmstein/yahnac/comments/CommentsParserTest.java
+++ app/src/test/java/com/malmstein/yahnac/comments/CommentsParserTest.java
@@ -17,9 +17,9 @@ public class CommentsParserTest {
 
     private static final String BASE_URI = "https://news.ycombinator.com/news";
 
-    private static final String TITLE_SAMPLE = "You are probably thinking of this article: <a href=\"http://www.dailydot.com/society/anonymous-website-challenge-voidnull-real-name/\" rel=\"nofollow\">http://www.dailydot.com/society/anonymous-website-challenge-...</a> which links to <a href=\"http://voidnull.sdf.org\" rel=\"nofollow\">http://voidnull.sdf.org</a>.";
-    private static final String AUTHOR_SAMPLE = "jenkstom";
-    private static final String TIME_AGO_SAMPLE = "12 hours ago";
+    private static final String TITLE_SAMPLE = "Location: Dubai, UAE<p>Remote: Yes</p><p>Willing to relocate: Yes, planning to move back to Europe in a few months (somewhere as of yet undecided, I'm originally from the UK)</p><p> Technologies: Ruby on Rails, JavaScript (Node, React, jQuery, Backbone), Erlang, PHP (Wordpress)</p><p>Résumé/CV: <a href=\"https://www.linkedin.com/profile/view?id=52691687\" rel=\"nofollow\">https://www.linkedin.com/profile/view?id=52691687</a>, <a href=\"https://github.com/lucaspiller\" rel=\"nofollow\">https://github.com/lucaspiller</a></p><p> Email: luca@stackednotion.com</p><p>I'm a full stack developer who seems to focus on maintaining legacy applications that need some love - it's not what I go out looking for, but they seem to find me! My last client project involved migrating a bunch of Rails 1 / 2 applications to new servers and upgrading Ruby / Rails versions to patch security issues. Before that I was tasked with fixing up a new Rails application, that had been built by an outsourcing team who didn't really have any clue what they were doing. I'm currently taking a sabbatical from client work to build my own products in the Wordpress space, but if something interesting comes along I'm happy to take it on!</p>";
+    private static final String AUTHOR_SAMPLE = "lucaspiller";
+    private static final String TIME_AGO_SAMPLE = "3 minutes ago";
     private static final String QUESTION_SAMPLE = "I'm a student developer, @pravj on GitHub. I try to follow the best possible development practice when I'm working like documentation, respecting language's style guide etc.<p>But one thing I don't like with me is that I never write tests.</p><p>Why I never followed that aspect is because I always felt that the thing that I'm writing, sounds totally OK.</p><p>One another negative point is that I don't actually get it that How to do/start this and what type of tests I should write for any given project. Maybe this is because I have never done this before.</p><p>For example recently I wrote a identicons library, Penticons[1]. It generates GitHub contribution flavored identicons. You can read the development story here[2].</p><p>Now, the thing that stopped me from writing tests was, I thought that ultimately it's a image generation library, why this thing needs any tests.</p><p>I don't want to lost my 1 point in the Joel score. Whenever I see a <i>build passing</i> label on a GitHub repository, It makes me crazy.</p><p>I'm asking here because I just love HN, I know people will suggest some awesome things here.</p><p>1. https://github.com/penticons/penticons.go</p><p>2. http://pravj.github.io/blog/penticons-the-hash-game/</p>";
     private static final String EMPTY_QUESTION_SAMPLE = "";
 
@@ -59,7 +59,7 @@ public class CommentsParserTest {
     @org.junit.Test
     public void returnsAllAskStoryComments() {
         Vector<ContentValues> storyComments = askStoryCommentsParser.parse();
-        assertEquals(31, storyComments.size());
+        assertEquals(53, storyComments.size());
     }
 
     @org.junit.Test
