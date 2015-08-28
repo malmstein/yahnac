4b3492978f4c19eefc8ee9acce90700518242d8d
David Gonzalez <malmstein@gmail.com>
2015-08-28 20:11:09 +0100

    binding listeners to ReplyView

--- app/src/main/java/com/malmstein/yahnac/comments/ReplyView.java
+++ app/src/main/java/com/malmstein/yahnac/comments/ReplyView.java
@@ -8,13 +8,18 @@ import android.widget.EditText;
 import android.widget.FrameLayout;
 
 import com.malmstein.yahnac.R;
+import com.malmstein.yahnac.login.InputFieldValidator;
 import com.novoda.notils.caster.Views;
 
 public class ReplyView extends FrameLayout {
 
+    private InputFieldValidator inputFieldValidator = new InputFieldValidator();
+
     private View cancel;
     private View reply;
     private EditText comment;
+
+    private Listener listener;
 
     public ReplyView(Context context) {
         super(context);
@@ -39,5 +44,42 @@ public class ReplyView extends FrameLayout {
         cancel = Views.findById(this, R.id.reply_cancel);
         reply = Views.findById(this, R.id.reply_send);
         comment = Views.findById(this, R.id.reply_comment);
+
+        cancel.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (listener != null) {
+                    listener.onReplyCancelled();
+                }
+            }
+        });
+
+        reply.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (listener != null) {
+                    if (validate()) {
+                        listener.onReplySent(comment.getText().toString());
+                    }
+                }
+            }
+        });
+    }
+
+    public void setListener(Listener listener) {
+        this.listener = listener;
+    }
+
+    private boolean validate() {
+        String commentText = comment.getText().toString();
+        boolean isCommentValid = inputFieldValidator.isValid(commentText);
+        return isCommentValid;
+    }
+
+    public interface Listener {
+        void onReplyCancelled();
+
+        void onReplySent(String text);
+
     }
 }
