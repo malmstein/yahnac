7427c4671668275cffdab18925b9af1cf3facec4
David Gonzalez <malmstein@gmail.com>
2016-01-10 19:07:34 +0000

    starting to enable data binding

---
+++ app/src/main/java/com/malmstein/yahnac/viewModel/StoryViewModel.java
@@ -0,0 +1,7 @@
+package com.malmstein.yahnac.viewModel;
+
+import android.databinding.BaseObservable;
+
+public class StoryViewModel extends BaseObservable {
+
+}
---
+++ app/src/test/java/com/malmstein/yahnac/viewModel/StoryViewModelTest.java
@@ -0,0 +1,16 @@
+package com.malmstein.yahnac.viewModel;
+
+import org.junit.Before;
+
+/**
+ * User: malmstein
+ * Date: 10/01/2016
+ * Time: 19:03
+ */
+public class StoryViewModelTest {
+
+    @Before
+    public void setUp() {
+
+    }
+}
